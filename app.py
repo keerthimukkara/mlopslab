545ba2851f950afe8432bc6f6e2f61fe173fe86b
keerthi <keerthimukkara18@gmail.com>
2026-04-14 20:00:12 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+import joblib
+import numpy as np
+
+app = FastAPI()
+
+# Load model
+model = joblib.load("model.pkl")
+
+@app.get("/")
+def home():
+    return {"message": "Wine Quality Prediction API"}
+
+@app.post("/predict")
+def predict(
+    fixed_acidity: float,
+    volatile_acidity: float,
+    citric_acid: float,
+    residual_sugar: float,
+    chlorides: float,
+    free_sulfur_dioxide: float,
+    total_sulfur_dioxide: float,
+    density: float,
+    pH: float,
+    sulphates: float,
+    alcohol: float
+):
+    features = np.array([[fixed_acidity, volatile_acidity, citric_acid,
+                          residual_sugar, chlorides, free_sulfur_dioxide,
+                          total_sulfur_dioxide, density, pH, sulphates, alcohol]])
+
+    prediction = model.predict(features)[0]
+
+    return {"predicted_quality": prediction}
